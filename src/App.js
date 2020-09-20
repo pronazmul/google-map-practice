aaaeed3400a38a3a4b5482bd886389bb73ffbf3f
Nazmul Huda <mhnazmul48@gmail.com>
2020-09-20 19:38:02 +0600

    react Google Map installation complete & Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {GoogleMap, withGoogleMap, withScriptjs} from 'react-google-maps'
+
 
 function App() {
+  
+function Map(){
+  return <GoogleMap defaultZoom={10} defaultCenter={{lat:22.841930, lng:89.558060}}/>
+}
+const wrappedMap = withScriptjs(withGoogleMap(Map))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div style={{height:'100vh', width:'100vw'}}>
+        <h1 className='jumbotron text-center py-3 display-3 font-width-bold'>Google Map Practice</h1>
+          <wrappedMap 
+          googleMapUrl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{height:'100%'}}/>}
+          containerElement={<div style={{height:'100%'}}/>}
+          mapElement={<div style={{height:'100%'}}/>}
+          />
+      </div>
   );
 }
 
